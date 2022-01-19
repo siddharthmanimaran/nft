895a4b3b5c7beadbd3fe04f3c09028057c395562
Siddharth <sid28071998@gmail.com>
2022-01-19 19:41:05 +0530

    set current drop

---
+++ pages/currentDrop.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CurrentDropContainer from "../features/CurrentDropdown/Container";
+const CurrentDrop = () => {
+  return (
+    <div>
+      <CurrentDropContainer />
+    </div>
+  );
+};
+
+export default CurrentDrop;
