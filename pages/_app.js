3b4d3a0452b4926904847580c370d20010e5f42c
Siddharth <sid28071998@gmail.com>
2022-01-19 19:40:03 +0530

    common changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {" "}
+      <Head>
+        <title>Keerthana</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>{" "}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
